7532ac1f3532df7f3ba5250e52d62288d3b91648
Rafael da Costa Vedana <rafael.vedana@outlook.com>
2025-03-31 21:44:29 -0300

    media queries starting

--- src/js/carrousel.js
+++ src/js/carrousel.js
@@ -1,16 +1,19 @@
 export function initCarousels() {
-  const carousels = ["pt", "mat", "geo", "his", "fis", "bio"]; // Removido ""
+  const carousels = ["pt", "mat", "geo", "his", "fis", "bio"] // Removido ""
 
-  carousels.forEach(id => {
-    const carouselElement = document.querySelector(`#task-carousel-${id}`);
+  carousels.forEach((id) => {
+    const carouselElement = document.querySelector(`#task-carousel-${id}`)
     if (carouselElement) {
       new Splide(carouselElement, {
         type: "normal",
         perPage: 2,
-        omitEnd:true,
-      }).mount();
+        omitEnd: true,
+        breakpoints: {
+         1200: {
+            perPage: 1,
+          },
+        },
+      }).mount()
     }
-  });
+  })
 }
-
-
