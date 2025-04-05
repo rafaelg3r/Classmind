a6ec5a8658210c780fd0d1d1e9d7011f11949be5
Rafael da Costa Vedana <rafael.vedana@outlook.com>
2025-04-05 20:31:39 -0300

    Versão 1.0 finalizada e operante

--- src/js/carrousel.js
+++ src/js/carrousel.js
@@ -1,19 +1,35 @@
 export function initCarousels() {
-  const carousels = ["pt", "mat", "geo", "his", "fis", "bio", "lit", "emp-ino", "dir-hum", "com-mar", "ing", "art", "qmc"] // Removido ""
+  const carousels = [
+    "pt",
+    "mat",
+    "geo",
+    "his",
+    "fis",
+    "bio",
+    "lit",
+    "emp-ino",
+    "dir-hum",
+    "com-mar",
+    "ing",
+    "art",
+    "qmc",
+  ]
 
   carousels.forEach((id) => {
     const carouselElement = document.querySelector(`#task-carousel-${id}`)
     if (carouselElement) {
       new Splide(carouselElement, {
         type: "normal",
-        perPage: 2,
+        perPage: 1,
         omitEnd: true,
-        breakpoints: {
-         1200: {
-            perPage: 1,
-          },
-        },
       }).mount()
     }
   })
+
+  const recentesCarousel = document.querySelector("#carousel-recentes")
+  if (recentesCarousel) {
+    new Splide(recentesCarousel, {
+      perPage: 5,
+    }).mount()
+  }
 }
